830891762f4d56a3a005472a1f268003926ed0d8
Joan Garriga <joangarrigaconesa@gmail.com>
2020-09-16 15:42:35 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+require 'open-uri'
+
+Transaction.destroy_all
+Artwork.destroy_all
+User.destroy_all
 
 puts 'Creating 10 fake users...'
 10.times do |i|
@@ -22,11 +27,13 @@ puts 'Creating 10 fake artworks...'
 10.times do
   artwork = Artwork.new(
     title:    Faker::Company.name,
-    description: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    description: "#{Faker::Address.street_address}, #{Faker::Address.city} Lorem blablabakbababanaababbabababa",
     price_per_hour:  rand(0..5),
     user: User.all.sample
 
   )
+  file = URI.open('https://images.unsplash.com/photo-1579762593131-b8945254345c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1722&q=80')
+  artwork.photo.attach(io: file, filename: 'artwork.png', content_type: 'image/png')
   artwork.save!
 end
 puts 'Finished!'
