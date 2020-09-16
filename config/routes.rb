006cf3217e7ba5394ab971f3c7be653dc65cd974
Joan Garriga <joangarrigaconesa@gmail.com>
2020-09-16 12:07:17 +0200

    book feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :artworks do
-    resources :transactions, only: [:create]
+    resources :transactions, only: [ :new, :create]
   end
 
 end
